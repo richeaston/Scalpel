168d98a799157ed267ef9c57f5d87a52bc4cecc5
RichE <rich.easton@gmail.com>
2025-02-06 22:06:45 +0000

    Sort-object

--- Dev/Uninstall-bloatapps.ps1
+++ Dev/Uninstall-bloatapps.ps1
@@ -1,7 +1,7 @@
-$bloatpackages = @('Copilot','Dev Home','Game Bar','Game Speech Window','Feedback hub','Microsoft 365 (Office)','Microsoft Bing Search','LinkedIn','Microsoft Clipchamp','Microsoft News','Microsoft OneDrive','Microsoft To Do','MSN Weather','Media Player','Outlook','OneDrive','Power Automate','Quick Assist','Solitaire & Casual Games','Sound Recorder','Sticky Notes','Teams','Windows Clock','Xbox TCUI', 'Xbox Identity Provider','xbox','Microsoft 365 Copilot')
+$bloatpackages = @('Copilot','Dev Home','Game Bar','Game Speech Window','Feedback hub','Microsoft Bing Search','LinkedIn','Microsoft Clipchamp','Microsoft News','Microsoft OneDrive','Microsoft To Do','MSN Weather','Media Player','Outlook','OneDrive','Power Automate','Quick Assist','Solitaire & Casual Games','Sound Recorder','Sticky Notes','Teams','Windows Clock','Xbox TCUI', 'Xbox Identity Provider','xbox','Microsoft 365 Copilot''Microsoft 365 (Office)')
 
 #uninstall via winget
-foreach ($app in $bloatpackages) {
+foreach ($app in ($bloatpackages | sort-object)) {
     try {
         Winget uninstall $app --silent --force --purge --verbose
     }
@@ -12,7 +12,7 @@ foreach ($app in $bloatpackages) {
 
 #Uninstall Appx Packages
 $appxpackages = @('MicrosoftWindows.CrossDevice', 'Microsoft.MicrosoftEdge.Stable', 'Microsoft.ZuneMusic', 'Microsoft.YourPhone', 'Microsoft.XboxGamingOverlay', 'Microsoft.GetHelp', 'Microsoft.Windows.DevHome' )
-foreach ($appx in $appxpackages) {
+foreach ($appx in ($appxpackages | Sort-object)) {
     Write-host "Trying to uninstalling $appx, Please Wait."
     Get-AppxPackage $appx -AllUsers -Verbose | Remove-AppxPackage -AllUsers -Verbose -ErrorAction Ignore
 }
